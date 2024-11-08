2daf248fc31b3f7a0d8a2408baa22e131a19d2c4
Bera Senol <bleuberry2be@icloud.com>
2024-11-08 19:19:41 +0100

    King Movement

--- src/piece/pieces/King.java
+++ src/piece/pieces/King.java
@@ -1,5 +1,7 @@
 package piece.pieces;
 
+import java.util.ArrayList;
+
 import board.Tile;
 import piece.Piece;
 import piece.PieceColor;
@@ -12,5 +14,29 @@ public class King extends Piece {
 	public King(PieceColor pieceColor, Tile tile) {
 		super(pieceColor, PieceType.KING, tile);
 		pieceImage = getPieceImage(PIECE_TYPE, pieceColor);
+	}
+
+	@Override
+	public ArrayList<Tile> getMoveableTiles() {
+		ArrayList<Tile> moveableTiles = new ArrayList<Tile>();
+		Tile[][] chessBoard = getChessBoard();
+		int file = this.getFile();
+		int rank = this.getRank();
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (isWithinBounds(file + i) && isWithinBounds(rank + j)
+						&& !chessBoard[file + i][rank + j].isPieceOnTile()) {
+					if (i != 0 || j != 0) {
+						moveableTiles.add(chessBoard[file + i][rank + j]);
+					}
+				}
+			}
+		}
+		return moveableTiles;
+	}
+
+	@Override
+	public ArrayList<Tile> getIndicatedTiles() {
+		return getMoveableTiles();
 	}
 }
