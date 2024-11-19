21ffaa9f019f5843a3404c53cba93d2096795e9e
Bera Senol <bleuberry2be@icloud.com>
2024-11-19 21:59:11 +0100

    Code Cleanup, King Capture

--- src/piece/pieces/King.java
+++ src/piece/pieces/King.java
@@ -37,13 +37,8 @@ public class King extends Piece {
 		int rank = this.getRank();
 		for (int i = -1; i <= 1; i++) {
 			for (int j = -1; j <= 1; j++) {
-				if (isWithinBounds(file + i) && isWithinBounds(rank + j) && (i != 0 || j != 0)) {
-					if (CHESS_BOARD[file + i][rank + j].isPieceOnTile()
-							&& !CHESS_BOARD[file + i][rank + j].getPiece()
-									.isPieceColorTurnColor()) {
-						// Adds Tiles around the King if PieceColor is not TurnColor
-						tile.add(CHESS_BOARD[file + i][rank + j]);
-					}
+				if (isCaptureable(file, rank, i, j)) {
+					tile.add(CHESS_BOARD[file + i][rank + j]);
 				}
 			}
 		}
@@ -203,11 +198,29 @@ public class King extends Piece {
 	}
 
 	private boolean isMoveable(int file, int rank, int i, int j) {
-		if (isWithinBounds(file + i) && isWithinBounds(rank + j)
-				&& !CHESS_BOARD[file + i][rank + j].isPieceOnTile() && (i != 0 || j != 0)) {
-			// Adds empty Tiles around the King
-			return true;
+		if (!isWithinBounds(file + i) || !isWithinBounds(rank + j)) {
+			return false;
 		}
-		return false;
+		if (CHESS_BOARD[file + i][rank + j].isPieceOnTile()) {
+			return false;
+		}
+		if (!(i != 0 || j != 0)) {
+			return false;
+		}
+		return true;
+	}
+
+	private boolean isCaptureable(int file, int rank, int i, int j) {
+		if (!isWithinBounds(file + i) || !isWithinBounds(rank + j)) {
+			return false;
+		}
+		if (!(i != 0 || j != 0)) {
+			return false;
+		}
+		if (!CHESS_BOARD[file + i][rank + j].isPieceOnTile()
+				|| CHESS_BOARD[file + i][rank + j].getPiece().isPieceColorTurnColor()) {
+			return false;
+		}
+		return true;
 	}
 }
